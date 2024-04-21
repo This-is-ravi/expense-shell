ece7e19efe74074724c10bee02bbc5a3d963c4e9
This-is-ravi <ravivenkat508@gmail.com>
2024-04-21 17:44:33 +0530

    expenses-shellscripting

--- frontend.sh
+++ frontend.sh
@@ -36,19 +36,19 @@ VALIDATE $? "Enabling nginx"
 systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE #here when there is deployment we r removing the old code(/* -->removing content of that folder)
 VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
-unzip /tmp/frontend.zip &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE # we removed the old zip file in folder,now it wil extract new file i
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copied expense conf" #create expense.conf file , bcz shell script cant  open vim file
+VALIDATE $? "Copied expense conf" #create expense.conf file , bcz shell script cant open vim file
 
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restarting nginx"
